fc8e95e94ff3b9d08540472d0b76a53feef8af51
FatherScorpion <FatherScorpion@outlook.jp>
2025-10-26 01:43:24 +0900

    fix

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,5 +1,4 @@
-
-import { Mastra } from '@mastra/core';
+import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { VercelDeployer }  from '@mastra/deployer-vercel';
 import { weatherWorkflow } from './workflows/weather-workflow';
