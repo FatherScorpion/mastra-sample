f76ebd2fef76b0cf537fa0c54ca1e670b94708d4
FatherScorpion <FatherScorpion@outlook.jp>
2025-10-26 00:58:28 +0900

    init

---
+++ .vercel/output/functions/index.func/tools.mjs
@@ -0,0 +1,3 @@
+import * as tool0 from './tools/a11886bf-f764-4eb4-9003-a1bf8f58f584.mjs';
+
+export const tools = [tool0]
--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,7 +1,7 @@
 
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
-import { LibSQLStore } from '@mastra/libsql';
+import { VercelDeployer }  from '@mastra/deployer-vercel';
 import { weatherWorkflow } from './workflows/weather-workflow';
 import { weatherAgent } from './agents/weather-agent';
 import { toolCallAppropriatenessScorer, completenessScorer, translationScorer } from './scorers/weather-scorer';
@@ -9,11 +9,8 @@ import { toolCallAppropriatenessScorer, completenessScorer, translationScorer } 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
   agents: { weatherAgent },
+  deployer: new VercelDeployer(),
   scorers: { toolCallAppropriatenessScorer, completenessScorer, translationScorer },
-  storage: new LibSQLStore({
-    // stores observability, scores, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
-  }),
   logger: new PinoLogger({
     name: 'Mastra',
     level: 'info',
